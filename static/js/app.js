197e1f2666c180eb160402155dab310363d249ff
shriparna <s_v_kamat@yahoo.com>
2023-06-11 02:56:37 -0700

    Completed draft bubble plot

--- static/js/app.js
+++ static/js/app.js
@@ -30,15 +30,13 @@ function dashboard(bdata) {
     // Get the metadata 
     metaData = Object.values(bdata.metadata);
     let choice = dropDown.property("value");
-    demographic(metaData, choice);
+    demographicAndGauge(metaData, choice);
 
     // Get the samples
     samples = Object.values(bdata.samples);
     sample = samples.filter(sample => sample.id == choice); 
     console.log(sample);
-    top10(sample);
-
-    plotBubble(sample);
+    top10AndBubble(sample);
 
     // Dynamically change the following based on the drop down selection
     dropDown.on("change", function() {
@@ -46,19 +44,17 @@ function dashboard(bdata) {
         newChoice = optionChanged(newChoice);
         console.log("choice: " + newChoice);
     
-        // Call a function to display the Demographic Info
-        demographic(metaData, newChoice);
+        // Call a function to display the Demographic Info and Gauge Plot
+        demographicAndGauge(metaData, newChoice);
 
-        // Call a function to display the plot of top 10 OTUs
+        // Call a function to display the plot of top 10 OTUs and Bubble Plot
         newSample = samples.filter(sample => sample.id == newChoice); 
         console.log(newSample);
-        top10(newSample);
-
-        // Call a function to display the bubble plot
-        plotBubble(newSample);
+        top10AndBubble(newSample);
     });
 };
 
+// This function builds the options for the drop down
 function buildOptions(items) {
 
     console.log("items");
@@ -68,7 +64,6 @@ function buildOptions(items) {
     console.log(items.length);
 
     for (let i= 0; i < totalItems; i++) {
-        // console.log(items[i]);
         options = dropDown.append("option");
         options.attr("value", items[i]).text(items[i]);
     }; 
@@ -79,8 +74,8 @@ function optionChanged(myChoice){
     return myChoice;
 }
 
-// This function populates the demographic data
-function demographic(mdata, mchoice) {
+// This function populates the demographic data and plots the Gauge
+function demographicAndGauge(mdata, mchoice) {
     console.log("Inside demographic");
     console.log(mdata);
     let metaDetails;
@@ -109,7 +104,8 @@ function demographic(mdata, mchoice) {
     });
 };
 
-function top10(bbsample){
+// This function plots the bar plot and the bubble plot
+function top10AndBubble(bbsample){
     // Locate the bar graph elemement from the index.html
     currentSample = bbsample[0];
     console.log("currentSample");
@@ -117,73 +113,45 @@ function top10(bbsample){
 
     let otuIds = Object.values(currentSample.otu_ids);
     let sampleValues =  Object.values(currentSample.sample_values);
-    let otuLabels =  Object.values(currentSample.otu_labels);
+    let otuLabels =  Object.values(currentSample.otu_labels).map(item => item.replace(/;/g, '<br>'));;
 
     let top10OtuIds = otuIds.slice(0,10).map(id => "OTU " + id).reverse();
     let top10SampleValues = sampleValues.slice(0, 10).reverse();
-    let top10OtuLables = otuLabels.slice(0, 10).reverse().map(item => item.replace(/;/g, '<br>'));
+    let top10OtuLables = otuLabels.slice(0, 10).reverse();
     console.log("top10OtuIds");
     console.log(top10OtuIds);
     console.log(top10OtuLables);
 
-    trace = {
-        x: top10SampleValues,
-        y: top10OtuIds,
-        text: top10OtuLables,
-        type: 'bar',
-        orientation: 'h',
-        hovertemplate: '%{text}<extra></extra>' //<extra> tag is used to remove trace0. Source: plotly.com
-    }
+    var traceBar = {
+            x: top10SampleValues,
+            y: top10OtuIds,
+            text: top10OtuLables,
+            type: 'bar',
+            orientation: 'h',
+            hovertemplate: '%{text}<extra></extra>' //<extra> tag is used to remove trace0. Source: plotly.com
+        };
 
-    data = [trace];
+    var dataBar = [traceBar];
 
-    Plotly.newPlot("bar", data);
+    Plotly.newPlot("bar", dataBar);
 
-};
+    var traceBubble = {
+            x: otuIds,
+            y: sampleValues,
+            text: otuLabels,
+            mode: 'markers',
+            marker: {
+                color: otuIds,
+                size: sampleValues
+            },
+            type: 'bubble'       
+        };
 
-function plotBubble(bbsample) {
-    var trace1 = {
-        x: [1, 2, 3, 4],
-        y: [10, 11, 12, 13],
-        mode: 'markers',
-        marker: {
-          color: ['hsl(0,100,40)', 'hsl(33,100,40)', 'hsl(66,100,40)', 'hsl(99,100,40)'],
-          size: [12, 22, 32, 42],
-          opacity: [0.6, 0.7, 0.8, 0.9]
-        },
-        type: 'scatter'
-    };
-      
-    var trace2 = {
-        x: [1, 2, 3, 4],
-        y: [11, 12, 13, 14],
-        mode: 'markers',
-        marker: {
-          color: 'rgb(31, 119, 180)',
-          size: 18,
-          symbol: ['circle', 'square', 'diamond', 'cross']
-        },
-        type: 'scatter'
-    };
-      
-    var trace3 = {
-        x: [1, 2, 3, 4],
-        y: [12, 13, 14, 15],
-        mode: 'markers',
-        marker: {
-          size: 18,
-          line: {
-            color: ['rgb(120,120,120)', 'rgb(120,120,120)', 'red', 'rgb(120,120,120)'],
-            width: [2, 2, 6, 2]
-          }
-        },
-        type: 'scatter'
-    };
-      
-    var data = [trace1, trace2, trace3];
-      
-    var layout = {showlegend: false};
-      
-    Plotly.newPlot('bubble', data, layout);
+    var dataBubble = [traceBubble];
 
+    var layoutBubble = {
+            showlegend: false
+        };
+      
+    Plotly.newPlot('bubble', dataBubble, layoutBubble);
 };
